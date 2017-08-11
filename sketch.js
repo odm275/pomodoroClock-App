d82112f735c8e315468f01fd3bbd98d6751caf20
odm275 <pomejia@gmail.com>
2017-08-11 12:41:08 -0500

    Added Bootsrap,parent() p5js function and style.css to style everything neatly

--- sketch.js
+++ sketch.js
@@ -2,27 +2,29 @@ function Pomodoro(){
   this.break = 5;//Standard braekTime in seconds;
   this.work = 25;//Standard workTime in seconds;
 }
-var pomodoro = new Pomodoro();
 
+var pomodoro = new Pomodoro();
 /*DOM */
 var timerMinute;//display MINUTES DOM
 var timerSecond;//display SECONDS DOM
 var workt;//DOM for work
 var breakt;//DOM for break
 
-var interval;//Create variable that holds timecount function;
-var time = false;//timer is not running
 var seconds = 60;
-
+var time = false; //time is not running at first
+var interval;//Create variable that holds timecount function;
 
 var boolWork = true;//Working Timer;
+
 var originalWork = pomodoro.work;
 var originalBreak = pomodoro.break;
 
 function setup() {
-  createCanvas(200,200);
+  noCanvas();
+  background(0);
   workt = createP('Work Time');
   breakt = createP('Break Time');
+  /*addwork,minuswork,addbreak,minusbreak,doTimer will be called multiple times for each buttonpress*/
 
   buttonaddWork = createButton('+');
   buttonaddWork.mousePressed(addwork);
@@ -41,7 +43,22 @@ function setup() {
 
   button = createButton('Start Timer');
   button.mousePressed(doTimer);//time can be true or false;
+  style();
+
 }
+/*Styling*/
+function style(){
+  workt.parent('displayWork');
+  breakt.parent('displayBreak');
+  buttonaddWork.parent('addWork');
+  buttonminusWork.parent('minusWork');
+  buttonaddBreak.parent('addBreak');
+  buttonminusBreak.parent('minusBreak');
+  timerMinute.parent('minutes');
+  timerSecond.parent('seconds');
+  button.parent('button');
+}
+
 /*Adjust settings*/
 function addwork(){
   pomodoro.work++;
@@ -65,6 +82,7 @@ function minusbreak(){
 }
 /*Run timer*/
 function doTimer(){
+
   if(time===false){//if timer is not running
     interval = setInterval(timeIt,1000);//execute every second;
     time = true;
@@ -106,8 +124,6 @@ function minutes(){
   }else{
     console.log('BreakTime');
     timerMinute.html(pomodoro.break);
-
   }
-
   timerSecond.html(seconds);
-  }
+}
